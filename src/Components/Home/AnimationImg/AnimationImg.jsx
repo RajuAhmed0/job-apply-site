abc4a69cdb34ac04ee7f83e51148a0e2e0641aec
RajuAhmed0 <itzraju98@gmail.com>
2025-03-24 13:55:04 +0600

    s

--- src/Components/Home/AnimationImg/AnimationImg.jsx
+++ src/Components/Home/AnimationImg/AnimationImg.jsx
@@ -4,13 +4,13 @@ import { motion } from "framer-motion";
 const AnimationImg = () => {
     return (
         <div>
-            <div className="relative flex justify-center text-center items-center h-screen bg-gray-100 overflow-hidden">
+         
+            <div className="relative flex justify-center items-center h-screen overflow-hidden">
                 {/* Back Image */}
                 <motion.img
                     src="https://i.ibb.co.com/nq2hRfyQ/developer-1.jpg"
                     alt="Background Image"
-                    className="absolute max-w-lg w-2/4 rounded-r-xl rounded-bl-xl shadow-lg top-0 left-11 "
-                    nitial={{ y: 200, opacity: 0 }}
+                    className="absolute max-w-full w-2/4 sm:w-3/4 md:w-1/2 lg:w-2/5 xl:w-1/3 rounded-r-xl rounded-bl-xl shadow-lg top-0 left-11"
                     animate={{ y: 100, opacity: 1 }}
                     transition={{ duration: 3, repeat: Infinity }}
                 />
@@ -19,12 +19,13 @@ const AnimationImg = () => {
                 <motion.img
                     src="https://i.ibb.co.com/Dg6LmGwZ/devoloper-2.jpg"
                     alt="Foreground Image"
-                    className="relative max-w-lg rounded-r-xl rounded-bl-xl shadow-xl top-28"
-                    nitial={{ x: -150, opacity: 0 }}
+                    className="relative max-w-full w-2/4 sm:w-3/4 md:w-1/2 lg:w-2/5 xl:w-1/3 rounded-r-xl rounded-bl-xl shadow-xl top-28"
                     animate={{ x: -100, opacity: 1 }}
                     transition={{ duration: 3, repeat: Infinity }}
                 />
             </div>
+
+
         </div>
     );
 };
